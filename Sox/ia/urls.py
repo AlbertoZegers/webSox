fe81ab02131eec71cc974905730b45a2c4dde79b
Alberto <al.zegers@duocuc.cl>
2023-06-21 23:38:28 -0400

    parte del CRUD terminada

--- Sox/ia/urls.py
+++ Sox/ia/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import visualizacion, feedback 
+from .views import visualizacion, feedback, modificar, eliminar 
 
 urlpatterns = [
     path('visualizacion/', visualizacion, name="visualizacion"),
     path('feedback/', feedback, name="feedback"),
+    path('modificar/<id>', modificar, name="modificar"),
+    path('eliminar/<id>', eliminar, name="eliminar")
 ]
--- Sox/ia/views.py
+++ Sox/ia/views.py
@@ -1,13 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ComentarioForm
+from .models import Comentario
 #import subprocess
 
 # Create your views here.
 def feedback(request):
-    data = {'form' : ComentarioForm} 
-    return render(request, 'ia/pagFeedback.html')
+    comentarios=Comentario.objects.all()
+    data = {'form' : ComentarioForm(), 'comentarios': comentarios}
+    if request.method == 'POST':
+         formulario=ComentarioForm(data=request.POST)
+         if formulario.is_valid():
+             formulario.save()
+         else:
+            formulario = ComentarioForm()
+    return render(request, 'ia/pagFeedback.html', data)
 
 def visualizacion(request):
     #comando = 'python manage.py shell -c "exec(open(\'Sox.py\').read())"'
     #subprocess.Popen(comando, shell=True)
     return render(request, 'ia/pagVisualizacion.html')
+
+def modificar(request, id):
+    comentario=get_object_or_404(Comentario, id=id)
+    data = {'form': ComentarioForm(instance=comentario)}
+    if request.method=='POST':
+        formulario= ComentarioForm(data=request.POST, instance=comentario)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('feedback')
+        else:
+           formulario=ComentarioForm() 
+    return render(request, 'ia/modificar.html', data)
+
+def eliminar(request, id):
+    comentario = get_object_or_404(Comentario, id=id)
+    comentario.delete()
+    return redirect('feedback')
