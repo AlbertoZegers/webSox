415492bd767ee5e0ba52ad67f4d1d5a4828d4c97
MatteoCerda <matt.cerda@duocuc.cl>
2023-06-21 20:53:12 -0400

    parte del CRUD avanzada

--- Sox/ia/models.py
+++ Sox/ia/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Comentario(models.Model):
+    mensaje = models.TextField()
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.mensaje
--- Sox/ia/views.py
+++ Sox/ia/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .forms import ComentarioForm
 #import subprocess
 
 # Create your views here.
 def feedback(request):
+    data = {'form' : ComentarioForm} 
     return render(request, 'ia/pagFeedback.html')
+
 def visualizacion(request):
     #comando = 'python manage.py shell -c "exec(open(\'Sox.py\').read())"'
     #subprocess.Popen(comando, shell=True)
